a525e3b4095663e89fd86e7369769288f0067721
vanessacai <1668576526@qq.com>
2025-02-13 10:02:09 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: '[name].[contenthash].js',      }
+        filename: 'index.[contenthash].js'     
+    },
+        plugins: [new HtmlWebpackPlugin({
+            title: '来吃饭啦',
+            template: 'src/assets/test.html'
+          }
+        )],
+        module: {
+            rules: [
+              {
+                test: /\.css$/i,
+                use: ["css-loader"],
+              },
+            ],
+          },
   };
